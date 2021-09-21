6e8b50ca5e1701fe20f98c8e73652c02830a77cb
Richard Browning <rbrowning@nanthealth.com>
2021-09-21 11:31:19 -0400

    feat(build): adding e2e tests back in

--- wdio.lambda.conf.js
+++ wdio.lambda.conf.js
@@ -59,10 +59,20 @@ module.exports.config = Object.assign({}, base, {
   capabilities,
   specs: ["./packages/@featherds/**/e2e/**/*.spec.js"],
   baseUrl: `${process.env.VUE_DEV_SERVER_URL}`,
-  reporters: process.env.GITHUB_TOKEN ? ["junit"] : ["spec"],
-  reporterOptions: {
-    outputDir: "e2e/reports/",
-  },
+  reporters: process.env.GITHUB_TOKEN
+    ? [
+        [
+          "junit",
+          {
+            outputDir: "./",
+            outputFileFormat: function (options) {
+              // optional
+              return `results-e2e.xml`;
+            },
+          },
+        ],
+      ]
+    : ["spec"],
   maxInstances: 3,
   specFileRetries: 3,
   framework: "mocha",
