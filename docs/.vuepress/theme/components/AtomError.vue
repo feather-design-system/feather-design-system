6b76d6e8d41d8fcc9c6f514d7ababf3b7310de93
Jonathan Millar <85951896+jmillar-nanthealth@users.noreply.github.com>
2021-11-29 15:41:09 +0000

    docs: added 404 page

---
+++ docs/.vuepress/theme/components/AtomError.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 152.016 171">
+    <defs>
+      <clipPath id="clip-path">
+        <rect id="Rectangle_1064" data-name="Rectangle 1064" width="152.016" height="171" fill="none"/>
+      </clipPath>
+    </defs>
+    <g clip-path="url(#clip-path)">
+      <path d="M40.746,130.936c-18.618,3.823-32.96,1.831-37.924-6.768-7.13-12.351,7.215-34.638,33.446-54.949" transform="translate(0.051 3.556)" stroke-linecap="round" stroke-width="2"/>
+      <path d="M73.622,116.384A173.726,173.726,0,0,1,46.458,126.57" transform="translate(2.387 5.979)" stroke-linecap="round" stroke-width="2"/>
+      <path d="M41.929,65.375c4.883-3.41,10.089-6.727,15.572-9.892,40.391-23.32,80.455-29.543,89.488-13.9S130.6,88.811,90.207,112.132q-3.753,2.167-7.5,4.135" transform="translate(2.154 1.693)" stroke-linecap="round" stroke-width="2"/>
+      <path d="M72.754,46.475c5.237,2.468,10.55,5.241,15.868,8.312q3.954,2.282,7.708,4.663" transform="translate(3.738 2.388)" stroke-linecap="round" stroke-width="2"/>
+      <path d="M46.458,36.572A163.129,163.129,0,0,1,64.535,42.78" transform="translate(2.387 1.879)" stroke-linecap="round" stroke-width="2"/>
+      <path d="M130.285,83.807c16.6,16.364,24.465,32.423,18.807,42.224-9.031,15.643-49.1,9.42-89.488-13.9S-6.209,57.226,2.822,41.582c4.945-8.563,19.19-10.574,37.7-6.813" transform="translate(0.051 1.693)" stroke-linecap="round" stroke-width="2"/>
+      <path d="M102.659,63.791A184.914,184.914,0,0,1,118.32,75.982" transform="translate(5.274 3.277)" stroke-linecap="round" stroke-width="2"/>
+      <path d="M41.367,94.376q-.18-4.4-.181-8.927C41.186,38.809,55.83,1,73.892,1,83.784,1,92.65,12.338,98.647,30.255" transform="translate(2.116 0.051)" stroke-linecap="round" stroke-width="2"/>
+      <path d="M101.018,127.47c-5.872,22.557-15.846,37.4-27.164,37.4-11.289,0-21.243-14.77-27.12-37.233a173.582,173.582,0,0,1-4.8-28.785" transform="translate(2.154 5.079)" stroke-linecap="round" stroke-width="2"/>
+      <path d="M98.131,36.572a189.986,189.986,0,0,1,5.542,47.049A199.046,199.046,0,0,1,100,122.558" transform="translate(5.042 1.879)" stroke-linecap="round" stroke-width="2"/>
+      <path class="circle" d="M72.538,103.86a4.825,4.825,0,0,1-3.524-1.46,4.7,4.7,0,0,1-1.451-3.513,4.644,4.644,0,0,1,1.451-3.481,4.927,4.927,0,0,1,6.983,0,4.75,4.75,0,0,1,1.513,3.481,4.81,4.81,0,0,1-.715,2.5,5.333,5.333,0,0,1-1.814,1.8,4.652,4.652,0,0,1-2.443.673m4.6-42.977L76.329,90.6H68.746l-.831-29.715Z" transform="translate(3.471 3.128)"/>
+    </g>
+  </svg>
+</template>
+<style lang="scss" scoped>
+@import "@featherds/styles/themes/variables";
+
+path {
+  fill: none;
+  stroke: var($secondary-variant);
+
+  &.circle {
+    stroke: none;
+    fill: var($primary-text-on-surface);
+  }
+}
+
+</style>
---
+++ docs/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,135 @@
+<template>
+  <FeatherPage class="layout 404">
+    <section>
+      <div class="section-wrapper">
+        <div class="not-found">
+          <AtomError />
+          <span>
+            <span class="overline">Error 404</span>
+            <h3>
+              Sorry! We can't find the page you're looking for.
+            </h3>
+            <FeatherButton as-anchor :href="$withBase('/')" primary>Return Home</FeatherButton>
+          </span>
+        </div>
+      </div>
+    </section>
+  </FeatherPage>
+</template>
+<script>
+import FeatherPage from "./FeatherPage.vue";
+import AtomError from "../components/AtomError.vue";
+import { FeatherButton } from "@featherds/button";
+
+export default {
+  components: {
+    FeatherPage,
+    AtomError,
+    FeatherButton
+  },
+  mounted() {
+  },
+  unmounted() {
+  },
+  methods: {
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "@featherds/styles/mixins/typography";
+@import "@featherds/styles/mixins/responsive";
+@import "@featherds/styles/themes/variables";
+
+section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  z-index: 0;
+  background-color: transparent;
+  min-height: 65vh;
+
+  &:before {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: var($hero-gradient-2-1), var($hero-gradient-2-2),
+      var($hero-gradient-2-3), var($hero-gradient-2-4);
+  }
+
+  .section-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 40px;
+    max-width: 600px;
+
+    span.overline {
+      @include overline();
+      color: var($secondary-variant);
+      margin-bottom: 12px;
+    }
+
+    h3 {
+      @include headline1();
+    }
+
+    .not-found {
+      padding: 40px 0;
+      display: flex;
+      flex-basis: 0;
+      flex-shrink: 0;
+      flex-grow: 1;
+
+      & > :deep(svg) {
+        flex: 1;
+        flex-basis: 100%;
+      }
+
+      & > span {
+        flex-direction: column;
+        display: flex;
+        align-items: flex-start;
+        justify-content: center;
+        padding-left: 40px;
+        padding-right: 20px;
+        min-width: 380px;
+
+        p {
+          padding-top: 0;
+          margin-top: 5px;
+          @include body-large();
+          color: var($secondary-text-on-surface);
+        }
+
+        a.btn {
+          margin-bottom: 0;
+        }
+      }
+      @media screen and (max-width: 599px) {
+        flex-direction: column;
+        text-align: center;
+
+        span {
+          padding: 0;
+          display: block;
+          min-width: auto;
+        }
+        :deep(svg) {
+          flex-basis: 140px;
+          margin-bottom: 32px;
+        }
+        h2 {
+          margin-bottom: 24px;
+        }
+        p {
+          margin-top: 0;
+        }
+      }
+    }
+  }
+}
+</style>
