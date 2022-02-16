1feb7d837f154b92ad0cc76c0b5e85071c87ac5a
Rik Browning <rbrowning@nanthealth.com>
2022-02-16 18:54:35 +0000

    test(snapshots): improving snapshots, closing #42

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   testEnvironment: "jsdom",
-
+  snapshotSerializers: ["<rootDir>/jest/serializer"],
   moduleFileExtensions: ["js", "jsx", "json", "vue"],
 
   transform: {
     "^.+\\.vue$": "@vue/vue3-jest",
     ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
       "jest-transform-stub",
-    "^.+\\.jsx?$": "babel-jest"
+    "^.+\\.jsx?$": "babel-jest",
   },
 
   transformIgnorePatterns: ["/node_modules/"],
@@ -15,12 +15,10 @@ module.exports = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
     "^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
-      "jest-transform-stub"
+      "jest-transform-stub",
   },
 
-  testPathIgnorePatterns: [
-    "<rootDir>/(?:.+?)/e2e/"
-  ],
+  testPathIgnorePatterns: ["<rootDir>/(?:.+?)/e2e/"],
 
   testMatch: ["**/@featherds/*/src/**/*.spec.(js|jsx|ts|tsx)"],
 
@@ -28,7 +26,7 @@ module.exports = {
 
   watchPlugins: [
     "jest-watch-typeahead/filename",
-    "jest-watch-typeahead/testname"
+    "jest-watch-typeahead/testname",
   ],
 
   testResultsProcessor: "jest-teamcity-reporter",
@@ -36,8 +34,8 @@ module.exports = {
 
   collectCoverageFrom: [
     "**/@featherds/*/src/**/*.{js,ts,vue}",
-    "!**/node_modules/**"
+    "!**/node_modules/**",
   ],
 
-  coverageReporters: ["text"]
+  coverageReporters: ["text"],
 };
