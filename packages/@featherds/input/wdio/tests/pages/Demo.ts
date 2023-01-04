72a9744cff6493bb6fc42f2ff96a62a1f9194c6b
Guillaume Chesnel <gchesnel@nanthealth.com>
2023-01-04 11:58:37 +0000

    test(input): fixed odd behavior related to page refresh for firefox

--- packages/@featherds/input/wdio/tests/pages/Demo.ts
+++ packages/@featherds/input/wdio/tests/pages/Demo.ts
@@ -3,6 +3,7 @@ const DISPLAY = "#display";
 import { FeatherInput } from "../../FeatherInput";
 export const create = async () => {
   await browser.url(`${process.env.VUE_DEV_SERVER_URL}/Input-Demo`);
+  await browser.refresh();
   await $(INPUT).waitForExist({ timeout: 60000 });
   const input = new FeatherInput(INPUT);
   return {
