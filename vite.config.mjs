4971a88fd7819afc13c0c2ef1ba46f82b2144b17
janderson_nha <janderson@nanthealth.com>
2024-10-11 16:05:54 -0400

    feat(icons): fix vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,4 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
-  },
 });
